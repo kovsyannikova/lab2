09abcea7e16ee6349b582d46cc4bd994ce347cdd
kovsyannikova <ksn_chimera@mail.ru>
2023-05-18 12:08:07 +0700

    Паттерн наблюдатель для отслеживания состояния переделан на сигнал-слотовую модель

--- FileObserver.h
+++ FileObserver.h
@@ -1,15 +1,22 @@
 #ifndef FILEOBSERVER_H
 #define FILEOBSERVER_H
 
-#include "IObserver.h"
+#include <QObject>
 
-class FileObserver : public IObserver
+// Наследуем класс наблюдателя от QObject для объявления слота
+// Интерфейс IObserver теперь не нужен
+class FileObserver : public QObject
 {
+Q_OBJECT
 private:
     qint64 size_ = -1; // размер файла. -1 если файл не существует
 
 public:
-    void UpdateState(qint64 size) override; // Переопределяем метод
+    FileObserver(QObject* parent = nullptr) : QObject(parent) {}
+
+public slots:
+    // Делаем обработчик уведомлений о состоянии файла слотом
+    void UpdateState(qint64 size);
 };
 
 
--- main.cpp
+++ main.cpp
@@ -1,16 +1,32 @@
-#include <thread>
 #include "File.h"
 #include "FileObserver.h"
 
-int main() {
-    File file("file.txt"); //Создаем объект File и указываем файл который будем отслеживать
-    FileObserver observer; //Создаем наблюдателя
-    file.Attach(&observer); //Подписываем наблюдателя на файл
+#include <thread>
+
+int main()
+{
+    File file("file.txt"); // Создаем объект File и указываем файл который будем отслеживать
+    FileObserver observer; // Создаем наблюдателя
+
+    // Вместо прикрепления наблюдателя FileObserver к издателю File
+    // Используем конструкцию QObject::connect для связи сигнала Notify и слота UpdateState
+
+    QObject::connect(&file, &File::Notify, &observer, &FileObserver::UpdateState);
+
+    // Если нужно будет подключать несколько наблюдателей
+    //    std::vector<FileObserver> observers(2);
+
+    //    for(auto& observer: observers)
+    //    {
+    //        QObject::connect(&file, &File::notify, &observer, &FileObserver::update);
+    //    }
+
 
     while (true)
     {
-        file.checkFile(); //Каждые 100 млс проверяем информацию о файле
+        file.checkFile(); // Каждые 100 млс проверяем информацию о файле
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
+
     return 0;
 }
